782861fb273b2b7a8de8f6266c893faf0c8d3c2a
Diogo Ferreira <diogo.rosas.ferreira@gmail.com>
2015-10-20 18:27:51 -0200

    Parcial para tela de Cadastro

--- cw-rest-web/src/main/webapp/app/resources/js/cw/login/register-ctrl.js
+++ cw-rest-web/src/main/webapp/app/resources/js/cw/login/register-ctrl.js
@@ -1,10 +1,11 @@
 angular.module('cw-app').controller('register-ctrl', ['$scope', '$http', '$window', function ($scope, $http, $window) {
-	$scope.register = function(){
-		console.log($scope.email_register);
-		console.log($scope.first_name_register);
+
+    $scope.register = function(){
+        console.log($scope.email_register);
+        console.log($scope.first_name_register);
         console.log($scope.surname_register);
         console.log($scope.birthdate_register);
         console.log($scope.password_register);
-	}
+    }
 }]);
 
