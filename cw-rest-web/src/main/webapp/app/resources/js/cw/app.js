e4656fadafc4d91e277973592a07166b3aea2b4e
Diogo Ferreira <diogo.rosas.ferreira@gmail.com>
2015-10-23 16:59:12 -0200

    Diretivas para Whiteboard

--- cw-rest-web/src/main/webapp/app/resources/js/cw/app.js
+++ cw-rest-web/src/main/webapp/app/resources/js/cw/app.js
@@ -1,1 +1,25 @@
-angular.module('cw-app', ['ui.bootstrap']);
+angular.module('cw-app', ['ui.bootstrap'])
+.directive('cwNavbar', function() {
+    return {
+        restrict: 'E',
+        templateUrl: '../../resources/js/cw/directives/navbar/navbar-template.html'
+    };
+})
+.directive('cwWhiteboard', function() {
+    return {
+        restrict: 'E',
+        templateUrl: '../../resources/js/cw/directives/whiteboard/whiteboard-template.html'
+    };
+})
+.directive('cwWhiteboardStages', function() {
+    return {
+        restrict: 'E',
+        templateUrl: '../../resources/js/cw/directives/whiteboard/stages-template.html'
+    };
+})
+.directive('cwWhiteboardTickets', function() {
+    return {
+        restrict: 'E',
+        templateUrl: '../../resources/js/cw/directives/whiteboard/tickets-template.html'
+    };
+});
