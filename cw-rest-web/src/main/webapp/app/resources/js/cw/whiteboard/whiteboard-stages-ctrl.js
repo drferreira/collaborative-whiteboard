03b36d273bc66b8dd162a12cda21f6ed5f3f4c33
Diogo Ferreira <diogo.rosas.ferreira@gmail.com>
2015-10-28 19:22:56 -0200

    Template para Ticket

--- cw-rest-web/src/main/webapp/app/resources/js/cw/whiteboard/whiteboard-stages-ctrl.js
+++ cw-rest-web/src/main/webapp/app/resources/js/cw/whiteboard/whiteboard-stages-ctrl.js
@@ -1,3 +1,4 @@
 angular.module('whiteboard-module').controller('WhiteboardStagesCtrl', ['$scope', '$http', '$window', '$rootScope', function ($scope, $http, $window, $rootScope) {
+
     console.log($scope.cwStages);
 }]);
--- cw-rest-web/src/main/webapp/app/resources/js/cw/whiteboard/whiteboard-ticket-ctrl.js
+++ cw-rest-web/src/main/webapp/app/resources/js/cw/whiteboard/whiteboard-ticket-ctrl.js
@@ -1,6 +1,10 @@
 angular.module('whiteboard-module').controller('WhiteboardTicketCtrl', ['$scope', '$http', '$window', '$rootScope', function ($scope, $http, $window, $rootScope) {
 
 
+    $scope.formatTime = function (time){
+        return new Date(time);
+    }
+
 }]).filter('unsafe', function($sce) {
     return $sce.trustAsHtml;
 });
