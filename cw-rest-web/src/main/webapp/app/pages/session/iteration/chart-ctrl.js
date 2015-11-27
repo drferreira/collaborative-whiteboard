b48dfaa61db71b379eabfc87557533724aa22dd1
Diogo Ferreira <diogo.rosas.ferreira@gmail.com>
2015-11-27 15:24:43 -0200

    Correção Chart Iteration

--- cw-rest-web/src/main/webapp/app/pages/session/iteration/chart-ctrl.js
+++ cw-rest-web/src/main/webapp/app/pages/session/iteration/chart-ctrl.js
@@ -2,56 +2,74 @@ angular.module('iteration-module').controller('ChartCtrl', ['$scope', '$http', f
     var HTTP_GET_ITERATIONS_URL = window.location.origin + '/cw-rest/session/rest/iteration/list';
     var HTTP_GET_STORIES_BY_ITERATION_URL = window.location.origin + '/cw-rest/session/rest/story/fetch';
 
+    initChartData();
 
-    $scope.$on("updateIterationChartData", function () {
-        populateChart();
+    $http.get(HTTP_GET_ITERATIONS_URL).then(function (response) {
+        $scope.iterations = response.data;
     });
 
-    function populateChart(){
+    $scope.$on("updateIterationChartData", function () {
+        $http.get(HTTP_GET_ITERATIONS_URL).then(function (response) {
+            $scope.iterations = response.data;
+        });
+    });
+
+    $scope.$watch('iterations', function () {
+        if($scope.iterations){
+            initChartData();
+
+            processIterations($scope.iterations);
+            $scope.data = [$scope.days, $scope.points, $scope.stories];
+        }
+    });
+
+    function initChartData(){
+        $scope.series = ['Dias', 'Pontos', 'Estorias'];
+        $scope.labels = [];
+        $scope.data = [];
         $scope.days = [];
         $scope.points = [];
         $scope.stories = [];
-        $scope.labels = [];
-        $scope.data = [];
-        $scope.series = ['Dias', 'Pontos', 'Estorias'];
-
-        $http.get(HTTP_GET_ITERATIONS_URL).then(function (response) {
-            angular.forEach(response.data, function(iteration, key) {
-                $scope.labels.push(iteration.name);
-                calcIterationDays(iteration);
-                calcStoryData(iteration);
-            });
-
-            $scope.data = [$scope.days, $scope.points, $scope.stories];
-        });
-    }
+    };
 
 
-    function calcIterationDays(iteration){
+    function calcIterationDays(iteration) {
         var init = moment(new Date(iteration.init));
         var end = moment(new Date(iteration.end));
         $scope.days.push(moment.duration(end.diff(init)).asDays());
     };
 
-    function calcStoryData(iteration){
-        var storyPoints = 0;
+    function processIterations(iterations) {
+        var iteration = iterations.shift();
 
+        calcStoryData(iteration, function () {
+            $scope.labels.push(iteration.name);
+            calcIterationDays(iteration);
+
+            if (iterations.length > 0)
+                processIterations(iterations);
+        });
+
+    }
+
+    function calcStoryData(iteration, callback) {
         $http.get(HTTP_GET_STORIES_BY_ITERATION_URL, {
             params: {
                 iterationName: iteration.name
             }
         }).then(function (response) {
-            var stories = response.data;
-            $scope.stories.push(stories.length);
+            var storyPoints = 0;
 
-            angular.forEach(stories, function (value, key) {
-                storyPoints = storyPoints + value.storyPoints;
+            angular.forEach(response.data, function (story, key) {
+                storyPoints += story.storyPoints;
 
             }, storyPoints);
 
+            $scope.stories.push(response.data.length);
             $scope.points.push(storyPoints);
+
+            if (callback)
+                callback();
         });
     };
-
-    populateChart();
 }]);
